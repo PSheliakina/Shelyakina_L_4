fe8202ebea8d4a821df51338ba6e58c5e8273502
PShelyakina <polinasheliakina@gmail.com>
2022-02-18 10:44:19 +0700

    Updated Average - Added Scanner

--- src/Average.java
+++ src/Average.java
@@ -1,29 +1,37 @@
+import java.util.Scanner;
+
 public class Average {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        double c = 4;
-        double d = 29;
+        System.out.println("Введите первое число диапозона");
+
+        Double number1 = scanner.nextDouble();
+
+        System.out.println("Введите второе число диапозона");
+
+        Double number2 = scanner.nextDouble();
+
+        double c = number1;
+        double d = number2;
         double sum = 0;
         double sumEven = 0;
         double counter = 0;
 
         for (double i = c; i <= d; i++) {
+            if (i % 2 == 0) {
+                sumEven = sumEven + i;
+                counter++;
+            }
             sum = sum + i;
         }
 
-        double result = sum / ((d - c) + 1);
-        System.out.println("Среднее арифметическое всех чисел " + result);
-
-        for (double i = c; i <= d; i++) {
-            if (i % 2 == 0) {
-                sumEven = sumEven + i;
-                counter ++;
-            }
-        }
-
         double resultEven = sumEven;
-        System.out.println("Среднее арифметичесткое четных чисел " + resultEven / counter);
+        double result = sum / ((d - c) + 1);
 
+        System.out.println("Среднее арифметическое всех чисел = " + result);
+
+        System.out.println("Среднее арифметичесткое четных чисел = " + resultEven / counter);
 
     }
 
